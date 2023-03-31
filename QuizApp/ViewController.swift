549ef637f3a2d6255d0c70525a88f3bf26e790b9
muratmnz <minazzmurat@gmail.com>
2023-04-01 00:10:29 +0300

    added ui objects , images and connected with UI

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueBtn: UIButton!
+    @IBOutlet weak var falseBtn: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func answerBtnPressed(_ sender: UIButton) {
+    }
+    
 }
 
