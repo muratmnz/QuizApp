756f57c59be39540b631904a5a0b0df2f4ad0b6d
muratmnz <minazzmurat@gmail.com>
2023-03-31 22:50:48 +0300

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Murat Minaz on 31.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
